298b07f85a63770bf2bc1b5d7f1cf91591bdbf14
Olliemint <furywrld5@gmail.com>
2022-03-25 00:49:55 +0300

    story images and accept and decline functionality

--- Js/scripts.js
+++ Js/scripts.js
@@ -239,11 +239,88 @@ closeBtn.addEventListener('click', () => {
     document.querySelector('.left').style.display = 'none';
 })
 
-let love = document.getElementById("love1");
-love.addEventListener("click", (e)=>{
+// let love = document.getElementById("btn1");
+// love.addEventListener("click", (e)=>{
+//     e.preventDefault()
+//    alert("yyy")
+// })
+
+
+// accept btn
+
+let button1 = document.getElementById("btn1");
+
+button1.addEventListener("click", (e) =>{
     e.preventDefault()
-    love.style. backgroundColor ="red"
+    
+
+    document.getElementById("decline").innerHTML = `You are now friends`
 })
 
+// decline button
+
+let button2 = document.getElementById("btn2");
+
+button2.addEventListener("click", (e) =>{
+    e.preventDefault()
+    
+
+    document.getElementById("decline").innerHTML = `Request removed`
+})
+// jquery
+
+$(document).ready(function(){
+    $("#btn1").click(function(event){
+        $("#btn2").hide();
+        $("#btn1").hide();
+
+    })
+    $("#btn2").click(function(event){
+        $("#btn2").hide();
+        $("#btn1").hide();
+
+    })
+
+
+    
+
+})
+let button3 = document.getElementById("btn3");
+
+button3.addEventListener("click", (e) =>{
+    e.preventDefault()
+    
+
+    document.getElementById("decline2").innerHTML = `You are now friends`
+})
+
+// decline button
+
+let button4 = document.getElementById("btn4");
+
+button4.addEventListener("click", (e) =>{
+    e.preventDefault()
+    
+
+    document.getElementById("decline2").innerHTML = `Request removed`
+})
+// jquery
+
+$(document).ready(function(){
+    $("#btn3").click(function(event){
+        $("#btn4").hide();
+        $("#btn3").hide();
+
+    })
+    $("#btn4").click(function(event){
+        $("#btn3").hide();
+        $("#btn4").hide();
+
+    })
+
+
+    
+
+})
 
 // THE END
